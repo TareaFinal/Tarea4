aced7e5665cecad05f7e9e6c33eb8e29a4107baf
TareaFinal <85418385+TareaFinal@users.noreply.github.com>
2021-06-17 08:10:30 -0300

    agrego funciones faltantes

--- scr/PartidaMultijugador.cpp
+++ scr/PartidaMultijugador.cpp
@@ -1,41 +1,76 @@
 #include "../partidaMultijugador.h"
+#include <string>
+#include <iostream>
+#include <time.h>
+using namespace std;
 
-PartidaMultijugador::PartidaMultijugador(int idPartida, string juego, bool transmitida) {
-	this->idPartida = idPartida;
-	this->duracion = NULL;
-	this->FechaInicio = NULL;
-	this->Videojuego = juego;
-	this->Transmitida = transmitida;
+	// Constructor
+PartidaMultijugador::PartidaMultijugador(int idPartida, DtFechaHora* duracion, DtFechaHora fechaInicio,string juego, Jugador* player, bool transmitida, 
+										Jugador sal1,DtFechaHora sal2) : Partida(idPartida,null,fechaInicio,juego,player)
+{
+	Transmitida=transmitida;
+	colComentarios=null;
 }
-
-void PartidaMultijugador::setTransmitida(bool transmit) {
+	// Destructor
+PartidaMultijugador::~PartidaMultijugador(){
+}
 	
-}
-
-void PartidaMultijugador::setSalidas(Jugador *jug,DtFechaHora *fecha) {
+	// Setters
+	void PartidaMultijugador::setTransmitida(bool transmit){
+		Transmitida=transmit;
+	}
 	
-}
-
-//Getters
-bool PartidaMultijugador::getTransmitida() {
-	return true;
-}
-//	Jugador,DtFechaHora *getSalidas();
-
-//operaciones
-float PartidaMultijugador::darTotalDeHorasParticipantes() {
-	return 0.0;	
-}
-
-set<DtJugador*> PartidaMultijugador::getDtJugadoresUnidos() {
-	set<DtJugador*> jugadores;
-	return jugadores;
-}
-
-void PartidaMultijugador::terminarPartida() {
+	/*void PartidaMultijugador::setsalida1(Jugador salid1){
+		salida1=salid1;
+	}
+	void setsalida2(DtFechaHora salid2){
+		salida2=salid2;
+	}*/
 	
-}
+	
+	// Getters
+	bool PartidaMultijugador::getTransmitida(){
+		return Transmitida;
+	}
+	Jugador PartidaMultijugador::getsalida1(){
+		return salida1;
+	}
+	DtFechaHora PartidaMultijugador::getsalida2(){
+		return salida2;
+	}
 
-DtPartida *PartidaMultijugador::getDataPartida() {
-	return NULL;
+	//funciones
+	float darTotalDeHorasParticipantes(){
+		float d=0;
+		DtFechaHora x;
+		set<DtJugador>::iterator it;
+		for (it = Conjunto.begin(); it != Conjunto.end(); ++it) {
+    		if (it->fecha!=null) {
+    			x=duracionDt(fechaInicial,fecha);
+    			d= d + x.getAnio()*365*24 + x.getMes()*30*24 + x.getDia()*24
+    			+ x.getHora() + x.getMinuto()/60;
+			} //recorro el conjunto de jugadores y salidas para la suma de duraciones;
+		}
+		return d;
+	}
+	set<DtJugador> getDtJugadoresUnidos(){
+		set<DtJugador>::iterator it;
+		for (it = Conjunto.begin(); it != Conjunto.end(); ++it) {
+    		conj.insert(*it.jug); //recorro el conjunto de jugadores y salidas para agregar al nuevo conjunto creado que devolveré
+		}
+		return conj;
+	}
+	void terminarPartida(){
+		DtFechaHora fechafin=getFechaSistema();
+		duracion=duracionDt(fechaInicial,fechafin); //llamo a la funcion auxiliar
+		set<DtJugador>::iterator it;
+		for (it = Conjunto.begin(); it != Conjunto.end(); ++it) {
+    		if(it.fecha==null) it.fecha=fechafin; //recorro el conjunto de Jugadores y si aun no ha terminado su partida la termino
+		}
+	}
+	DtPartida getDataPartida(){
+		DtPartidaMultijugador dtpm=DtPartidaMultijugador(duracion,FechaInicio,player.getNick(),Videojuego, 
+								Transmitida, string *nicknameJugadoresUnidos, colComentarios);
+
+	}
 }
